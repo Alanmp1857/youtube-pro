5af07a392e16a86b7107d751c55f881250e50d3b
Alanmp1857 <alan.panicker-bvcoel@bvp.edu.in>
2023-11-17 22:36:06 +0530

    added message bar to live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import { generateRandomName, makeRandomMessage } from "../utils/helper";
 
 const LiveChat = () => {
+  const [liveMessage, setLiveMessage] = useState("");
+
   const dispatch = useDispatch();
 
   const chatMessages = useSelector((store) => store.chat.messages);
@@ -24,14 +26,52 @@ const LiveChat = () => {
     return () => {
       clearInterval(polling);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <div className="m-2 p-2 h-[524px] border border-black w-full bg-slate-100 overflow-y-scroll flex flex-col-reverse">
-      {chatMessages.map((c, i) => (
-        <ChatMessage key={i} name={c.name} message={c.message} />
-      ))}
-    </div>
+    <>
+      <div className="m-2 p-2 h-[524px] border border-black w-full bg-slate-100 overflow-y-scroll flex flex-col-reverse">
+        <div>
+          {chatMessages.map((c, i) => (
+            <ChatMessage key={i} name={c.name} message={c.message} />
+          ))}
+          <form
+            className="flex p-2 border border-slate-300"
+            onSubmit={(e) => {
+              e.preventDefault();
+              console.log("ON form submit", liveMessage);
+              dispatch(
+                addMessage({
+                  name: "Alan Panicker",
+                  message: liveMessage,
+                })
+              );
+            }}>
+            <img
+              className="h-7 w-7 rounded-full"
+              src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png"
+              alt=""
+            />
+            <input
+              type="text"
+              className="py-1 pl-2 ml-3 w-3/4 border border-black rounded-md"
+              value={liveMessage}
+              onChange={(e) => {
+                setLiveMessage(e.target.value);
+              }}
+            />
+            <button>
+              <img
+                className="ml-6 h-7 w-7"
+                src="https://img.icons8.com/android/96/paper-plane.png"
+                alt="send"
+              />
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
